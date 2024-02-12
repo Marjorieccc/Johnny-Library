132d530263cd78ae7acfae973b39c089fc17a817
Dan (Wai Tan Wong) <120222461+Dan-Wong56004@users.noreply.github.com>
2024-02-12 02:58:36 -0500

    Update navBar.tsx

--- src/component/Navbar/navBar.tsx
+++ src/component/Navbar/navBar.tsx
@@ -2,76 +2,77 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../searchBar/searchBar";
 
-export default function NavBar() {
-  return (
-    <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-4">
-      <div className="pl-4 flex items-center">
-        <Link
-          to="/"
-          className="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl"
-        >
-          Unionville Library
-        </Link>
-      </div>
-      
-      {/* Hidden menu for small screen users */}
-      <div className="block lg:hidden pr-4">
-        <button
-          id="nav-toggle"
-          className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-purple-500 appearance-none focus:outline-none"
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
+type NavbarProps = {
+    onSearch: (term: string) => void;
+  };
+  
 
-      {/* Menu for large screen */}
-      <div className="w-full flex-grow lg:flex lg:content-center lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 z-20">
-        <SearchBar />
-        <ul className="list-reset lg:flex justify-end items-center">
-          <li className="mr-3 py-2 lg:py-0">
-            <Link
-              to="/"
-              className="inline-block py-2 px-4 text-gray-900 font-bold no-underline"
-            >
-              Home
-            </Link>
-          </li>
-          <li className="mr-3 py-2 lg:py-0">
-            <details>
-              <summary>
-                <Link to="explore" className="mr-3 py-2 lg:py-0">
-                  Explore
-                </Link>
-              </summary>
-              <ul>
+export default function Navbar({ onSearch }: NavbarProps) {
+
+    const [isExploreOpen, setIsExploreOpen] = useState(false);
+    const [isServiceOpen, setIsServiceOpen] = useState(false);
+    const exploreDropdownRef = useRef<HTMLDivElement>(null);
+    const serviceDropdownRef = useRef<HTMLDivElement>(null);
+
+    const handleClickOutside = (e: MouseEvent) => {
+        if (exploreDropdownRef.current && !exploreDropdownRef.current.contains(e.target as Node)) {
+            setIsExploreOpen(false);
+          }
+          if (serviceDropdownRef.current && !serviceDropdownRef.current.contains(e.target as Node)) {
+            setIsServiceOpen(false);
+          }
+      };
+    
+      useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }, []);
+
+      const handleExploreClick = () => {
+        setIsExploreOpen(!isExploreOpen);
+        setIsServiceOpen(false); 
+      };
+    
+      const handleServiceClick = () => {
+        setIsServiceOpen(!isServiceOpen);
+        setIsExploreOpen(false);
+      };
+    
+
+  return (
+    <nav className="fixed w-full z-10 top-0 bg-white border-b border-gray-400">
+        <div className="container mx-auto flex flex-wrap items-center justify-between py-4">
+            <div className="flex items-center flex-1">
+            <MainHeader />
+            </div>
+
+            <SearchBar onSearch={onSearch} />
+
+            <ul className="list-reset lg:flex justify-end items-center flex-1">
                 <li className="mr-3 py-2 lg:py-0">
-                  <Link
-                    to="explore"
-                    className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-2 px-4"
-                  >
-                    Book Category 1
-                  </Link>
+                    <div tabIndex={0} role="button" className="dropdown btn m-1 inline-block text-gray-600  hover:text-gray-900 py-2 px-4" onClick={handleExploreClick} ref={exploreDropdownRef}>Explore!</div>
+                    {isExploreOpen && (
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-gray-100 rounded-lg w-full absolute left-0 right-0">
+                        <li><a>Fiction</a></li>
+                        <li><a>Non-Fiction</a></li>
+                        </ul>
+                    )}
                 </li>
                 <li className="mr-3 py-2 lg:py-0">
-                  <Link
-                    to="explore"
-                    className="inline-block text-gray-600 no-underline hover:text-gray-900 hover:underline py-2 px-4"
-                  >
-                    Book Category 2
-                  </Link>
+                <div tabIndex={0} role="button" className="dropdown btn m-1 inline-block text-gray-600  hover:text-gray-900 py-2 px-4" onClick={handleServiceClick} ref={serviceDropdownRef}>Service</div>
+                    {isServiceOpen && (
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-gray-100 rounded-lg w-full absolute left-0 right-0">
+                            <li><a>Book Study Room</a></li>
+                            <li><a>Book a Computer</a></li>
+                        </ul>
+                    )}
                 </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      </div>
-    </div>
+            </ul>
+        </div>
+    </nav>
   );
-}
+};
+
+
