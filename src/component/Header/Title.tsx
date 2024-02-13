b6da017e7a68336d41d81f7372514b60767b9fe1
Dan (Wai Tan Wong) <120222461+Dan-Wong56004@users.noreply.github.com>
2024-02-12 20:08:02 -0500

    Create Title.tsx

---
+++ src/component/Header/Title.tsx
@@ -0,0 +1,8 @@
+export default function Title(){
+    return (
+        <header >
+            <h1 className="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl">
+            Markham Library
+            </h1>
+        </header>);
+}
