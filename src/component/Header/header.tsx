d00b044438dd3ddb266c626414eaad16cb9b143b
Dan (Wai Tan Wong) <120222461+Dan-Wong56004@users.noreply.github.com>
2024-02-12 21:38:51 -0500

    Update header.tsx

--- src/component/Header/header.tsx
+++ src/component/Header/header.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useState } from 'react';
+import Title from "../Header/Title";
 import NavBar from "../navbar/navBar";
+import SearchBar from "../SearchBar/searchBar"
 
 export default function Header() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleSearch = (term: string) => {setSearchTerm(term);};
+
   return (
-    <header className="fixed w-full z-10 top-0 bg-white border-b border-gray-400">
-      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-4">
-        <NavBar />
+      <div className="fixed w-full z-10 top-0 bg-white border-b border-gray-400">
+        <div className="container mx-auto flex flex-wrap items-center justify-between py-4"> 
+          <MainHeader />
+          <SearchForm onSearch= {handleSearch}/>
+          <Navbar />
+        </div>
       </div>
-    </header>
   );
 }
