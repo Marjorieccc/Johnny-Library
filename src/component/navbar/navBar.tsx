1713314b4ba3a610c5d6ffa8af6c23ee065722ef
Wai Tan Wong <wwong85@mySeneca.ca>
2024-02-13 19:26:20 -0500

    update hover problem

--- src/component/navbar/navBar.tsx
+++ src/component/navbar/navBar.tsx
@@ -20,7 +20,8 @@ export default function Navbar() {
         <li className="mr-3 py-2 lg:py-0">
           <div
             onMouseEnter={handleExploreHover}
-            onMouseLeave={handleExploreHover}
+            onMouseLeave={handleExploreHover}>
+            <div
             tabIndex={0}
             role="button"
             className="dropdown btn m-1 inline-block text-gray-600  hover:text-gray-900 py-2 px-4"
@@ -28,11 +29,13 @@ export default function Navbar() {
             Explore!
           </div>
           {isExploreDropdownVisible && <Dropdown navString="explore" />}
+          </div>
         </li>
         <li className="mr-3 py-2 lg:py-0">
           <div
             onMouseEnter={handleServiceHover}
-            onMouseLeave={handleServiceHover}
+            onMouseLeave={handleServiceHover}>
+            <div
             tabIndex={0}
             role="button"
             className="dropdown btn m-1 inline-block text-gray-600  hover:text-gray-900 py-2 px-4"
@@ -40,6 +43,7 @@ export default function Navbar() {
             Service
           </div>
           {isServiceDropdownVisible && <Dropdown navString="service" />}
+          </div>
         </li>
       </ul>
     </div>
