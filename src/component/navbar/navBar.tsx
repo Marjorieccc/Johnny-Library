4bacbcb8ca5abc634de3baf95ac96bb3c2edd281
Wai Tan Wong <wwong85@mySeneca.ca>
2024-02-13 19:51:13 -0500

    solved hover problem

--- src/component/navbar/navBar.tsx
+++ src/component/navbar/navBar.tsx
@@ -20,26 +20,28 @@ export default function Navbar() {
         <li className="mr-3 py-2 lg:py-0">
           <div
             onMouseEnter={handleExploreHover}
-            onMouseLeave={handleExploreHover}
-            tabIndex={0}
-            role="button"
-            className="dropdown btn m-1 inline-block text-gray-600  hover:text-gray-900 py-2 px-4"
-          >
-            Explore!
+            onMouseLeave={handleExploreHover}>
+              <div
+              tabIndex={0}
+              role="button"
+              className="dropdown btn m-1 inline-block text-gray-600  hover:text-gray-900 py-2 px-4">
+              Explore!
+              </div>
+                {isExploreDropdownVisible && <Dropdown navString="explore" />}
           </div>
-          {isExploreDropdownVisible && <Dropdown navString="explore" />}
         </li>
         <li className="mr-3 py-2 lg:py-0">
           <div
             onMouseEnter={handleServiceHover}
-            onMouseLeave={handleServiceHover}
-            tabIndex={0}
-            role="button"
-            className="dropdown btn m-1 inline-block text-gray-600  hover:text-gray-900 py-2 px-4"
-          >
-            Service
+            onMouseLeave={handleServiceHover}>
+              <div
+              tabIndex={0}
+              role="button"
+              className="dropdown btn m-1 inline-block text-gray-600  hover:text-gray-900 py-2 px-4">
+              Service
+              </div>
+                {isServiceDropdownVisible && <Dropdown navString="service" />}
           </div>
-          {isServiceDropdownVisible && <Dropdown navString="service" />}
         </li>
       </ul>
     </div>
