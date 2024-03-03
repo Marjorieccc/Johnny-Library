5fd3ba3496bdf9d66f3fea5b5dcf60568b60587f
Wai Tan Wong <wwong85@mySeneca.ca>
2024-03-03 02:13:56 -0500

    change search function

--- client/src/api/fetch.ts
+++ client/src/api/fetch.ts
@@ -1,4 +1,4 @@
-import data from "./resources.json";
+import data from './resources.json';
 
 export type Resource = {
   resource_id: string;
@@ -17,6 +17,28 @@ export type Resource = {
   return_date: string | null;
   status: string;
 };
+
+export interface IMedium {
+  _id: string,
+  format: string,
+  publisher: string,
+  language: string[],
+  year_of_publication: number,
+  return_date: Date | null,
+  status: string
+}
+
+export interface IResource {
+  _id: string, 
+  title: string,
+  thumbnail_url?: string,
+  cover_url?: string,
+  audience: string,
+  category: string[],
+  shortDescription?: string,
+  longDescription?: string,
+  medium: IMedium[];
+}
 
 // fetch categories of resources
 export async function fetchCategories(): Promise<string[]> {
@@ -127,19 +149,17 @@ export async function fetchByFilter(
 }
 
 // fetch matching resources with resource title or resource author as keyword
-export async function searchResource( searchTerm : string ) {
-  console.log("Fetching resource for:", searchTerm);
-  let filteredResource : Resource[]= []; 
+export async function fetchBySearchTerm( searchTerm : string ):Promise<IResource[]>{
+  console.log('Fetching resource for:', searchTerm);
+  let searchResult = [];
   try {
-      const response = await fetch('/resources.json');
-      const data = await response.json();
-      filteredResource = data.resources.filter((searchResult: Resource) => 
-          searchResult.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
-          searchResult.author.some(author => author.toLowerCase().includes(searchTerm.toLowerCase()))
-      );  
-
+      const response = await fetch(`http://localhost:8080/resources/search?title=${searchTerm}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    searchResult = await response.json();
   } catch (error) {
-  console.error("Error fetching resource:", error);
+  console.error('Error fetching resource:', error);
 }
-  return filteredResource;
+  return searchResult;
 }
--- client/src/component/searchBar/searchBar.tsx
+++ client/src/component/searchBar/searchBar.tsx
@@ -1,35 +1,27 @@
 import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-type SearchProps = {
-  onSearch: (term: string) => void;
-};
-
-export default function SearchBar({ onSearch }: SearchProps) {
+export default function SearchBar() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if(inputRef.current){
-      onSearch(inputRef.current.value);
-    }
-    if(inputRef.current) { 
-      inputRef.current.value = ''; // clean up reference for new search
+    if (inputRef.current) {
+      navigate('/explore', { state: { searchTerm: inputRef.current.value } });
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className="flex-grow relative">
-    <input
-      type="search"
-      ref={inputRef} 
-      placeholder="Search"
-      className="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-neutral-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"
-    />
-    <div className="absolute search-icon" style={{ top: '0.375rem', left: '1rem' }}>
-      <svg className="fill-current pointer-events-none text-gray-800 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-        <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
-      </svg>
-    </div>
+      <input type="text" ref={inputRef} placeholder="Search"
+        className="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-neutral-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"
+      />
+      <div className="absolute search-icon" style={{ top: '0.375rem', left: '1rem' }}>
+        <svg className="fill-current pointer-events-none text-gray-800 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
+        </svg>
+      </div>
   </form>
   );
 }
