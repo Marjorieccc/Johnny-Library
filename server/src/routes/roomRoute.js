d0a0e8a8263a846ff941ab2c440a405fc62eaa20
Johnny C <johnny.chung2046@gmail.com>
2024-03-23 10:36:45 -0400

    delete extra js files

--- server/src/routes/roomRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var TimeTable_1 = require("../controllers/TimeTable");
-var roomRouter = (0, express_1.Router)();
-// get all the resources object from database
-roomRouter.get("/", TimeTable_1.default.getTimeTableInfo);
-exports.default = roomRouter;
