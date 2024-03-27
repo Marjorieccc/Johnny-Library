25e709c44808f36d2370e3792a42db958b74e834
Johnny C <johnny.chung2046@gmail.com>
2024-03-26 21:03:59 -0400

    add auth0 login

--- client/src/component/header/header.tsx
+++ client/src/component/header/header.tsx
@@ -1,27 +1,29 @@
-import React,  { useState } from 'react';
-import Title from './title';
-import NavBar from '../navbar/navBar';
-import SearchBar from '../searchBar/searchBar'
-import AccountLoginButton from '../account/accountLoginButton';
-import ReserveCheckButton from '../account/reserveCheckButton';
+import React, { useState } from "react";
+import Title from "./title";
+import NavBar from "../navbar/navBar";
+import SearchBar from "../searchBar/searchBar";
+import AccountLoginButton from "../account/accountLoginButton";
+import ReserveCheckButton from "../account/reserveCheckButton";
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Header() {
-
+  const { isAuthenticated, user } = useAuth0();
   return (
-    <div className="fixed w-full z-10 top-0 bg-white border-b border-gray-400">
-        <div className="container mx-auto flex items-center justify-between py-4 space-x-4"> 
-          <Title />
-          <div className="flex-grow"></div>
-          <div className="flex-grow flex items-center space-x-4">
-          <SearchBar/>
-          <AccountLoginButton/>
-          <ReserveCheckButton/>
-          </div>
-
-        </div>
-        <div className="container mx-auto flex items-center justify-between py-1 space-x-4"> 
-          <NavBar />
+    <div className="fixed top-0 z-10 w-full border-b border-gray-400 bg-white">
+      <div className="container mx-auto flex items-center justify-between space-x-4 py-4">
+        <Title />
+        <div className="flex-grow"></div>
+        <div className="flex flex-grow items-center space-x-4">
+          <SearchBar />
+          <span>{isAuthenticated ? user && user.name : "Guest "} </span>
+          <AccountLoginButton />
+          <ReserveCheckButton />
         </div>
       </div>
+      <div className="container mx-auto flex items-center justify-between space-x-4 py-1">
+        <NavBar />
+      </div>
+    </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 import HomePage from "./pages/home";
 import HomeContent from "./pages/homeContent";
@@ -8,17 +9,33 @@ import ExplorePage from "./pages/explore";
 import ResourceDetailPage from "./pages/resourceDetailPage";
 import "./index.css";
 
+import auth0ConfigJson from "./auth_config.json";
+
+const auth0Config = {
+  domain: auth0ConfigJson.domain,
+  clientId: auth0ConfigJson.clientId,
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+  },
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />}>
-        <Route index element={<HomeContent />} />
-        <Route path="explore" element={<ExplorePage />} />
-        <Route path="resource/:resource_id" element={<ResourceDetailPage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <Auth0Provider {...auth0Config}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<HomeContent />} />
+          <Route path="explore" element={<ExplorePage />} />
+          <Route
+            path="resource/:resource_id"
+            element={<ResourceDetailPage />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Auth0Provider>,
 );
