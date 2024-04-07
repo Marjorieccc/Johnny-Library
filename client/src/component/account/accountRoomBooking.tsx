a23f8f32a17629103d2a995eca49c644374ce03e
Johnny C <johnny.chung2046@gmail.com>
2024-04-07 17:23:16 -0400

    add room booking record in account

--- client/src/component/account/accountRoomBooking.tsx
+++ client/src/component/account/accountRoomBooking.tsx
@@ -2,14 +2,35 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { useAccountDetails } from "../../context/AccountDetailsProvider";
+import Auth0LoginRedirectBtn from "../auth0/Auth0LoginRedirectBtn";
+
+const listStyle = `
+  border 
+  border-gray-300 
+  rounded-md 
+  overflow-hidden`;
 
 export default function AccountRoomBooking() {
-  const { reservations } = useAccountDetails();
+  const { roomBookingRecords } = useAccountDetails();
   const { isAuthenticated } = useAuth0();
-
   return (
     <div>
-      <p>Add Room Booking List here</p>
+      {isAuthenticated && roomBookingRecords && (
+        <ul className={listStyle}>
+          {roomBookingRecords.map((booking) => {
+            const startDate = new Date(booking.timeSlot.start);
+            const endDate = new Date(booking.timeSlot.end);
+            return (
+              <li key={booking._id}>
+                <p>BookingID: {booking._id}</p>
+                <p>Timeslot: {startDate.toISOString()} - {endDate.toISOString()}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      {isAuthenticated && roomBookingRecords.length === 0 && <p>No Booking</p>}
+      {!isAuthenticated && <Auth0LoginRedirectBtn />}
     </div>
   );
 }
