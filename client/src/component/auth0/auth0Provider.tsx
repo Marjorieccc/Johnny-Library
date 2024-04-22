285d54fff54a950295c3d509ce2cce649e0f4e28
Johnny C <johnny.chung2046@gmail.com>
2024-04-22 10:55:47 -0400

    change to vite, auth0 secret

--- client/src/component/auth0/auth0Provider.tsx
+++ client/src/component/auth0/auth0Provider.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
-import auth0ConfigJson from "./auth_config.json";
+import auth0ConfigJson from "./authConfig.json";
 
 const auth0Config = {
-  domain: auth0ConfigJson.domain,
-  clientId: auth0ConfigJson.clientId,
+  domain: auth0ConfigJson.domain || "",
+  clientId: auth0ConfigJson.clientId || "",
   authorizationParams: {
     redirect_uri: window.location.origin,
-    audience: auth0ConfigJson.audience,
-    scope: auth0ConfigJson.scope,
+    audience: auth0ConfigJson.audience || "",
+    scope: auth0ConfigJson.scope || "",
   },
 };
 
-export default function Auth0ProviderWithHistory({ children }: { children: React.ReactNode;}){
+export default function Auth0ProviderWithHistory({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const navigate = useNavigate();
 
   function onRedirectCallback(appState: any) {
     const returnTo = appState?.returnTo || window.location.origin;
-    const baseUrl = "http://localhost:3000";
+    const baseUrl = "http://localhost:5713";
     const relativePath = returnTo.replace(baseUrl, "");
     navigate(relativePath);
   }
@@ -34,4 +38,3 @@ export default function Auth0ProviderWithHistory({ children }: { children: React
     </Auth0Provider>
   );
 }
-
---
+++ client/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Index from "./index.js";
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>,
+);
