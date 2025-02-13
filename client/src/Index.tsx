f080bffe9d4d6d13bd2e198640cf582d3bc2331a
Marjorieccc <bluerain345@yahoo.com.hk>
2025-02-12 20:26:33 -0500

    fix: fix React browser router

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,45 +1,52 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
+import HomePage from './pages/Home';
+import HomeContent from './pages/HomeContent';
+import ExplorePage from './pages/Explore';
+import ResourceDetailPage from './pages/ResourceDetail';
+import AccountPage from './pages/Account';
+import NotFoundPage from './pages/NotFound';
+import RoomFilter from './pages/service/Room';
+import RoomDetails from './pages/service/RoomDetails';
+import AccountReservation from './component/account/AccountReservation';
+import AccountRoomBooking from './component/account/AccountRoomBooking';
+import Auth0ProviderWithHistory from './component/auth0/Auth0Provider';
+import AccountDetailsProvider from './context/AccountDetailsProvider';
 
-import HomePage from "./pages/Home";
-import HomeContent from "./pages/HomeContent";
-import ExplorePage from "./pages/Explore";
-import ResourceDetailPage from "./pages/ResourceDetail";
-import Auth0ProviderWithHistory from "./component/auth0/Auth0Provider";
-import AccountDetailsProvider from "./context/AccountDetailsProvider";
-import AccountPage from "./pages/Account";
-import NotFoundPage from "./pages/NotFound";
-import RoomFilter from "./pages/service/Room";
-import RoomDetails from "./pages/service/RoomDetails";
-import AccountReservation from "./component/account/AccountReservation";
-import AccountRoomBooking from "./component/account/AccountRoomBooking";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={
+        <Auth0ProviderWithHistory>
+          <AccountDetailsProvider>
+            <HomePage />
+          </AccountDetailsProvider>
+        </Auth0ProviderWithHistory>
+      }
+    >
+      <Route index element={<HomeContent />} />
+      <Route path='explore' element={<ExplorePage />} />
+      <Route path='services/rooms' element={<RoomFilter />} />
+      <Route path='services/rooms/:id' element={<RoomDetails />} />
+      <Route path='resource/:resource_id' element={<ResourceDetailPage />} />
 
+      <Route path='account' element={<AccountPage />}>
+        <Route index element={<Navigate replace to='reservation' />} />
+        <Route path='reservation' element={<AccountReservation />} />
+        <Route path='roomBooking' element={<AccountRoomBooking />} />
+      </Route>
+
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 export default function Index() {
-  return (
-    <BrowserRouter>
-      <Auth0ProviderWithHistory>
-        <AccountDetailsProvider>
-          <Routes>
-            <Route path="/" element={<HomePage />}>
-              <Route index element={<HomeContent />} />
-              <Route path="explore" element={<ExplorePage />} />
-              <Route path="services/rooms" element={<RoomFilter />} />
-              <Route path="services/rooms/:id" element={<RoomDetails />} />
-              <Route
-                path="resource/:resource_id"
-                element={<ResourceDetailPage />}
-              />
-              <Route path="account" element={<AccountPage />}>
-                <Route index element={<Navigate replace to="reservation" />} />
-                <Route path="reservation" element={<AccountReservation />} />
-                <Route path="roomBooking" element={<AccountRoomBooking />} />
-              </Route>
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </AccountDetailsProvider>
-      </Auth0ProviderWithHistory>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"
 import Index from "./Index";
 import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Index />
+  <React.StrictMode>   
+          <Index />
   </React.StrictMode>,
 );
