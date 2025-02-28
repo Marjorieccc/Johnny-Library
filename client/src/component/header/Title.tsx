015d1cfbe64672500591f877509228469748d14f
Marjorieccc <bluerain345@yahoo.com.hk>
2025-02-28 01:45:14 -0500

    perf: optimize images

--- client/src/component/header/Title.tsx
+++ client/src/component/header/Title.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import Modal from '../modal/Modal';
-import SideNavBar from '../navbar/SideNavBar';
+import { Link } from "react-router-dom";
+import Modal from "../modal/Modal";
+import SideNavBar from "../navbar/SideNavBar";
 
 export default function Title() {
   return (
@@ -8,16 +8,17 @@ export default function Title() {
       <Modal
         modalContent={
           <div className="h-full my-10 overflow-x-hidden overflow-y-auto font-roboto-condensed">
-          <SideNavBar/>
-          </div>}
+            <SideNavBar />
+          </div>
+        }
         header={<p className="text-3xl font-bold">Navigation</p>}
         buttonContent={<span className="pi pi-bars"></span>}
         buttonCss="mx-2 text-xl lg:hidden"
       />
-            <img src="/logo.webp" alt="Logo" className="w-10 mr-1 lg:mr-0 lg:w-12"/>
+      <img src="/logo.webp" alt="Logo" className="w-10 mr-1 lg:mr-0 lg:w-12" />
       <h1 className="pt-1 mt-2 text-base text-gray-900 no-underline lg:text-xl lg:ml-3 font-fjalla lg:mb-1 lg:text-md ">
-        <Link to='/'>Johnny Library</Link>
+        <Link to="/">Johnny Library</Link>
       </h1>
     </header>
-    );
+  );
 }
